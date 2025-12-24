0afe941e73494ef2fe84438ca83f26fc33c99937
XHTimmo <xhtimmo@outlook.com>
2025-12-24 17:49:45 +0800

    fix: fix timer reg name

--- components/timer/src/timer.c
+++ components/timer/src/timer.c
@@ -5,10 +5,10 @@
 
 void delay_us(uint32_t val){
 #ifdef CONFIG_STARRYSKY_C1
-    REG_TIM0_CONFIG = (uint32_t)0x0100;
-    REG_TIM0_DATA = (uint32_t)(CONFIG_CPU_FREQ_MHZ * val - 1);
-    REG_TIM0_CONFIG = (uint32_t)0x0101; // irq disable, count down, continuous mode, timer enable
-    while(REG_TIM0_DATA != 0)
+    REG_TIM_0_CONFIG = (uint32_t)0x0100;
+    REG_TIM_0_DATA = (uint32_t)(CONFIG_CPU_FREQ_MHZ * val - 1);
+    REG_TIM_0_CONFIG = (uint32_t)0x0101; // irq disable, count down, continuous mode, timer enable
+    while(REG_TIM_0_DATA != 0)
         ;
 #elif CONFIG_STARRYSKY_L3
 
@@ -25,9 +25,9 @@ void delay_s(uint32_t val){
 
 void sys_tick_init(void){
 #ifdef CONFIG_STARRYSKY_C1
-    REG_TIM1_CONFIG = (uint32_t)0x0100;
-    REG_TIM1_DATA = (uint32_t)0xFFFFFFFF;
-    REG_TIM1_CONFIG = (uint32_t)0x0101; // irq disable, count up, continuous mode, timer enable
+    REG_TIM_1_CONFIG = (uint32_t)0x0100;
+    REG_TIM_1_DATA = (uint32_t)0xFFFFFFFF;
+    REG_TIM_1_CONFIG = (uint32_t)0x0101; // irq disable, count up, continuous mode, timer enable
 #elif CONFIG_STARRYSKY_L3
 
 #endif
@@ -35,7 +35,7 @@ void sys_tick_init(void){
 
 uint32_t get_sys_tick(void){
 #ifdef CONFIG_STARRYSKY_C1
-    return 0xFFFFFFFF - REG_TIM1_DATA;
+    return 0xFFFFFFFF - REG_TIM_1_DATA;
 #elif CONFIG_STARRYSKY_L3
     return 0;
 #endif
