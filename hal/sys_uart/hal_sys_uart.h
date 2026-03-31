b6353d09c8c2b7e39c62ebbcdd6bac01aa78f117
XH_Timmo <xhtimmo@outlook.com>
2026-03-31 17:10:59 +0800

    feat: add descript

--- hal/sys_uart/hal_sys_uart.h
+++ hal/sys_uart/hal_sys_uart.h
@@ -3,8 +3,24 @@
 
 #include <stdint.h>
 
+/**
+ * @brief 初始化系统串口
+ * 
+ */
 void hal_sys_uart_init(void);
+
+/**
+ * @brief 发送一个字符到系统串口
+ * 
+ * @param c 要发送的字符
+ */
 void hal_sys_putchar(char c);
+
+/**
+ * @brief 发送一个字符串到系统串口
+ * 
+ * @param str 要发送的字符串
+ */
 void hal_sys_putstr(char *str);
 
 #endif
