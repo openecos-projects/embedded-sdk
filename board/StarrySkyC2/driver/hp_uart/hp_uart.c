5732216306b5f6353eb99a2ba8302921cd75bf4b
Krismile233 <2649282398@qq.com>
2026-04-08 17:20:24 +0800

    feat:hp_uart for c2

--- board/StarrySkyC2/driver/hp_uart/hp_uart.c
+++ board/StarrySkyC2/driver/hp_uart/hp_uart.c
@@ -1,54 +1,25 @@
 #include "hal_hp_uart.h"
+#include "hal_hp_uart_type.h"
 #include "generated/autoconf.h"
 #include "board.h"
+#include <stdint.h>
 
-uint8_t hp_uart_hal_config(uint8_t uart_id, hp_uart_config_t* config){
-    if(uart_id != HAL_UART_PORT_0) {
-        return 1;
+void hal_hp_uart_init(void){
+    REG_UART_1_DIV = (uint32_t)(CONFIG_CPU_FREQ_MHZ * 1000000 / CONFIG_UART_BAUD_RATE);
+}
+
+void hal_hp_uart_putchar(char c){
+    while((REG_UART_1_LSR & 0x20) == 0);
+    REG_UART_1_TRX = (uint32_t)c;
+}
+
+void hal_hp_uart_putstr(char *str){
+    while (*str != '\0'){
+        hal_hp_uart_putchar(*str++);
     }
-    // 禁用UART
-    REG_UART_1_LCR = 0x00;
-
-    // 设置波特率
-    REG_UART_1_DIV = (CONFIG_CPU_FREQ_MHZ * 1000000 / config->baudrate) - 1;
-
-    // 启用FIFO和接收中断
-    REG_UART_1_FCR = 0x0F;  
-    REG_UART_1_FCR = 0x0C;
-
-    // 设置数据位，校验位，停止位
-    REG_UART_1_LCR = (config->data_bits - 5) << 0 |
-                     (config->parity << 1) |
-                     (config->stop_bits << 2);
-        
-    return 0;
 }
 
-uint8_t hp_uart_hal_send(uint8_t uart_id, char c){
-    while (((REG_UART_1_LSR & 0x100) >> 8) == 1);
-    REG_UART_1_TRX = c;
-    return 0;
-}
-
-uint8_t hp_uart_hal_send_str(uint8_t uart_id, char* str){
-    while (*str){
-        hp_uart_hal_send(0, *str++);
-    }
-    return 0;
-}
-
-uint8_t hp_uart_hal_recv(uint8_t uart_id, char* c){
-    while (((REG_UART_1_LSR & 0x100) >> 8) == 0);
-    *c = REG_UART_1_RRX;
-    return 0;
-}
-
-uint8_t hp_uart_hal_recv_str(uint8_t uart_id, char* str){
-    while (1){
-        hp_uart_hal_recv(uart_id, str++);
-        if (*str == '\n'){
-            break;
-        }
-    }
-    return 0;
+char hal_hp_uart_getchar(void){
+    while((REG_UART_1_LSR & 0x01) == 0);
+    return (char)REG_UART_1_TRX;
 }
