789273a320f86d47c54d04573794dfb2e689b80f
XHTimmo <84488727+XHTimmo@users.noreply.github.com>
2026-04-09 18:10:34 +0800

    Revert "Dev2.0"

--- components/gpio/include/gpio.h
+++ components/gpio/include/gpio.h
@@ -32,7 +32,7 @@ void gpio_set_direction(gpio_num_t gpio_num, gpio_mode_t direction);
  */
 void gpio_set_level(gpio_num_t gpio_num, gpio_level_t level);
 
-/**q
+/**
  * @brief GPIO获取电平函数
  * 
  * 获取指定GPIO引脚的当前电平。
@@ -40,7 +40,7 @@ void gpio_set_level(gpio_num_t gpio_num, gpio_level_t level);
  * @param gpio_num GPIO引脚号，枚举类型gpio_num_t
  * @return int32_t GPIO电平，0表示低电平，1表示高电平
  */
-int32_t gpio_get_level(gpio_num_t gpio_num)
+int32_t gpio_get_level(gpio_num_t gpio_num);
 
 /**
  * @brief GPIO配置单个引脚复用功能
