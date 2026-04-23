bca9ea01f70f26cdbfe43552425304bc7e3b7ebf
FINALxxx <3085378853@qq.com>
2026-04-23 16:48:57 +0800

    feat(libc): use logger in assert.h

--- components/libc/include/assert.h
+++ components/libc/include/assert.h
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include "stdio.h"
 
+
+#define USE_TIMMOLOG 1
+
 #ifndef CONFIG_BUILD_DEBUG
     #define assert(expr, info) ((void)0)
 #else
@@ -11,7 +14,11 @@
         ({ \
             typeof(expr) result = (expr); \
             if (!(expr)) { \
+#ifdef USE_TIMMOLOG
                 printf("(%s:%d) %s", __FILE__, __LINE__,(info)); \
+#else
+                log_warn("%s",(info)); \
+#endif
             } \
             result; \
         })
