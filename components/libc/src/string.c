303b220372ddae84d65d9c2cabad1ecb3d075fda
scratch-er <scratcher@foxmail.com>
2026-06-14 03:10:20 +0800

    fix(libc): fix `memcmp` signature different from standard.

--- components/libc/src/string.c
+++ components/libc/src/string.c
@@ -33,18 +33,16 @@ size_t strlen(const char *s) {
     return len;
 }
 
-size_t memcmp(const void* ptr1, const void* ptr2, size_t num) {
-    const unsigned char* p1 = (const unsigned char*)ptr1;
-    const unsigned char* p2 = (const unsigned char*)ptr2;
-    
-    while (num--) {
-        if (*p1 != *p2) {
-            return (*p1 - *p2);
+int memcmp(const void* ptr1, const void* ptr2, size_t num) {
+    const unsigned char *p1 = (const unsigned char*)ptr1;
+    const unsigned char *p2 = (const unsigned char*)ptr2;
+    for (size_t i=0; i<num; ++i) {
+        if (p1[i] > p2[i]) {
+            return 1;
+        } else if (p1[i] < p2[i]) {
+            return -1;
         }
-        p1++;
-        p2++;
     }
-    
     return 0;
 }
 
